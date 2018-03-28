c2b2ed5b52ba5998903ea22bd671bd23d3353dd4
Collin Johnston <collin.j.johnston@gmail.com>
2018-03-28 10:49:14 -0400

    logic for incrementing clock fully functional

--- main.asm
+++ main.asm
@@ -11,60 +11,72 @@ TC4	  EQU $58
 TC1	  EQU $52
 ;***************
 
-org $1000
-DW ahourten; the tens place for the alarm hour time
-DW ahourone; the ones place for the alarm hour time
-DW aminone
-DW aminten
-DW chourten; hour tens place for current time
-DW chourone
-DW cminten
-DW cminone
+	org $1000
+ahourten	 DS 1 ; the tens place for the alarm hour time
+ahourone	 DS 1 ; the ones place for the alarm hour time
+aminone		 DS 1 
+aminten		 DS 1 
+chourten	 DS 1; hour tens place for current time
+chourone	 DS 1
+cminten		 DS 1
+cminone		 DS 1
 
-org $400
-LDS #$2000
-
+	org $400
+	LDS #$2000
+	LDAA #%10010000
+	STAA TSCR1
+	LDAA #0
+	STAA cminone
+	STAA cminten
+	STAA chourone
+	STAA chourten
+TOP		 JSR IncMinute
+		 BRA TOP
 
 
 
 IncMinute:
-LDAA cminone
-INCA
-STAA cminone;increase the minute
-CMPA #!10
-BEQ addtotensmin;if we are at ten we need to increase the tens value
-RTS
+		  LDAA cminone
+		  INCA
+		  STAA cminone;increase the minute
+		  CMPA #!10
+		  BEQ addtotensmin;if we are at ten we need to increase the tens value
+		  RTS
+
 addtotensmin:
-LDAA #0
-STAA cminone;make the ones place 0
-LDAA cminten
-INCA
-STAA cminten
-CMPA #!6
-BEQ IncHour
+	 LDAA #0
+	 STAA cminone;make the ones place 0
+	 LDAA cminten
+	 INCA
+	 STAA cminten
+	 CMPA #!6
+	 BEQ IncHour
+	 RTS
 
 IncHour:
-LDAA #0
-STAA cminten;only need to reset the tens becaues tens increment routine resets the ones
-LDAA chourone
-INCA
-CMPA #!10
-BEQ addtohourtens
-LDAB chourten
-CMPD $402; if it is at 24 we need to reset
-BEQ resetDay
-STAA chourone
-RTS
+	LDAA #0
+	STAA cminten;only need to reset the tens becaues tens increment routine resets the ones
+	LDAA chourone
+	INCA
+	STAA chourone
+	CMPA #!10
+	LDAB chourten
+	CPD #$402; if it is at 24 we need to reset
+	BEQ resetDay
+	BEQ addtohourtens
+	RTS
+
 addtohourtens: LDAA #!0
-STAA chourone
-LDAA chourten
-INCA
-STAA chourten
-RTS
-resetDay:
-LDAA #0
-STAA chourten
-STAA chourone
+			   STAA chourone
+			   LDAA chourten
+			   INCA
+			   STAA chourten
+			   RTS
+
+resetDay:	   LDAA #0
+			   STAA chourten
+			   STAA chourone
+			   RTS
 
 
 
